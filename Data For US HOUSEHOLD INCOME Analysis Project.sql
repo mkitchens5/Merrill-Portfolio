85542fb49be173be7a07877f0fff9c5616bc5eaf
Merrill Kitchens <merrillkitchens5@gmail.com>
2025-03-05 03:53:42 -0500

    Update Data For US HOUSEHOLD INCOME Analysis Project.sql

--- Data For US HOUSEHOLD INCOME Analysis Project.sql
+++ Data For US HOUSEHOLD INCOME Analysis Project.sql
@@ -19,51 +19,53 @@ Limit 5;                     -- Shows the states with Top 5 most water
 
 -- Finding who has the lowest and highest household income
 
-SELECT u.State_Name, ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
-FROM us_project.us_household_income u 
-INNER Join us_project.ushouseholdincome_statistics us
-	ON u.id = us.id 
-WHERE Mean <> 0
+SELECT u.State_Name, 
+       ROUND(AVG(us.Mean), 1) AS Avg_Mean, 
+       ROUND(AVG(us.Median), 1) AS Avg_Median
+FROM us_project.us_household_income u
+INNER JOIN us_project.ushouseholdincome_statistics us 
+    ON u.id = us.id
+WHERE us.Mean <> 0
 GROUP BY u.State_Name
-ORDER BY 2 ASC         
-LIMIT 10       -- Shows who has the top 10 lowest incomes per household on average
- ;
+ORDER BY Avg_Mean ASC
+LIMIT 10;    -- Shows who has the top 10 lowest incomes per household on average
 
- SELECT u.State_Name, ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+
+ SELECT u.State_Name, ROUND(AVG(us.Mean),1), ROUND(AVG(us.Median),1)
 FROM us_project.us_household_income u 
 INNER Join us_project.ushouseholdincome_statistics us
 	ON u.id = us.id 
-WHERE Mean <> 0
+WHERE us.Mean <> 0
 GROUP BY u.State_Name
 ORDER BY 3 ASC         
 LIMIT 10       -- Shows who has the MOST top 10 lowest incomes per household on average
  ;
 
-SELECT u.State_Name, ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+SELECT u.State_Name, ROUND(AVG(us.Mean),1), ROUND(AVG(us.Median),1)
 FROM us_project.us_household_income u 
 INNER Join us_project.ushouseholdincome_statistics us
 	ON u.id = us.id 
-WHERE Mean <> 0
+WHERE us.Mean <> 0
 GROUP BY u.State_Name
 ORDER BY 2 DESC      
 LIMIT 10           -- Shows who has the top 10 highest incomes per household on average
  ;
  
-SELECT u.State_Name, ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+SELECT u.State_Name, ROUND(AVG(us.Mean),1), ROUND(AVG(us.Median),1)
 FROM us_project.us_household_income u 
 INNER Join us_project.ushouseholdincome_statistics us
 	ON u.id = us.id 
-WHERE Mean <> 0
+WHERE us.Mean <> 0
 GROUP BY u.State_Name
 ORDER BY 3 DESC      
 LIMIT 10           -- Shows who has the MOST top 10 highest incomes per household on average
  ;
  
-SELECT Type, COUNT(Type), ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+SELECT Type, COUNT(Type), ROUND(AVG(us.Mean),1), ROUND(AVG(us.Median),1)
 FROM us_project.us_household_income u 
 INNER Join us_project.ushouseholdincome_statistics us
 	ON u.id = us.id 
-WHERE Mean <> 0
+WHERE us.Mean <> 0
 GROUP BY Type
 HAVING COUNT(Type) > 20
 ORDER BY 3 DESC      
@@ -71,11 +73,11 @@ LIMIT 5            -- Shows who has the top 5 highest incomes by the type of hou
  ;
 
 
-SELECT Type, COUNT(Type), ROUND(AVG(Mean),1), ROUND(AVG(Median),1)
+SELECT Type, COUNT(Type), ROUND(AVG(us.Mean),1), ROUND(AVG(us.Median),1)
 FROM us_project.us_household_income u 
 INNER Join us_project.ushouseholdincome_statistics us
 	ON u.id = us.id 
-WHERE Mean <> 0
+WHERE us.Mean <> 0
 GROUP BY Type
 HAVING COUNT(Type) > 20
 ORDER BY 3       
@@ -83,11 +85,11 @@ LIMIT 5            -- Shows who has the top 5 lowest incomes by the type of hous
  ;
 
 -- Finding City Income Averages in United States
-SELECT u.State_Name, City, ROUND(AVG(Mean),1) as city_income_avg, ROUND(AVG(Median),1) as city_medianincome_avg
+SELECT u.State_Name, City, ROUND(AVG(us.Mean),1) as city_income_avg, ROUND(AVG(us.Median),1) as city_medianincome_avg
 FROM us_project.us_household_income u 
 INNER Join us_project.ushouseholdincome_statistics us
 	ON u.id = us.id 
-WHERE Mean <> 0
+WHERE us.Mean <> 0
 GROUP BY u.State_Name, City
 ORDER BY 3 DESC      
 LIMIT 15           -- Shows who has the MOST top 15 highest incomes per city on average
